ff59f71213c1cbedb0d189c27f1acc365b24222a
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-06-25 16:56:53 -0300

    Moving Overview example from master branch to gh-pages

---
+++ javascripts/data.js
@@ -0,0 +1,23 @@
+'use strict';
+
+window.DATA = [
+  { id:  1, name:  'A', counter: 0, email: 'a@mail.com' },
+  { id:  2, name:  'B', counter: 0, email: 'b@mail.com' },
+  { id:  3, name:  'C', counter: 0, email: 'c@mail.com' },
+  { id:  4, name:  'D', counter: 0, email: 'd@mail.com' },
+  { id:  5, name:  'E', counter: 0, email: 'e@mail.com' },
+  { id:  6, name:  'F', counter: 0, email: 'f@mail.com' },
+  { id:  7, name:  'G', counter: 0, email: 'g@mail.com' },
+  { id:  8, name:  'H', counter: 0, email: 'h@mail.com' },
+  { id:  9, name:  'I', counter: 0, email: 'i@mail.com' },
+  { id: 10, name: 'AA', counter: 0, email: 'aa@mail.com' },
+  { id: 11, name: 'BB', counter: 0, email: 'bb@mail.com' },
+  { id: 12, name: 'CC', counter: 0, email: 'cc@mail.com' },
+  { id: 13, name: 'DD', counter: 0, email: 'dd@mail.com' },
+  { id: 14, name: 'EE', counter: 0, email: 'ee@mail.com' },
+  { id: 15, name: 'FF', counter: 0, email: 'ff@mail.com' },
+  { id: 16, name: 'GG', counter: 0, email: 'gg@mail.com' },
+  { id: 17, name: 'HH', counter: 0, email: 'hh@mail.com' },
+  { id: 18, name: 'II', counter: 0, email: 'ii@mail.com' },
+  { id: 19, name: 'JJ', counter: 0, email: 'jj@mail.com' }
+];
---
+++ javascripts/overview_example.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var app = angular.module('OverviewExample', [ 'ngTableAsync' ]);
+
+app.controller('OverviewExampleController', function($scope, $q, $timeout, $window){
+
+  var simulatePromiseDelay = function(cb){
+    var defer = $q.defer();
+    $timeout(function(){
+      var result = cb();
+      defer.resolve(result);
+    }, 2500);
+    return defer.promise;
+  };
+
+  $scope.tableOptions = {
+    pageSize: 7,
+    getPage: function(skip, limit){
+      var page = $window.DATA.slice(skip, skip + limit);
+      return $q.all([
+        $window.DATA.length,
+        page
+      ]);
+    },
+    actions: {
+      increment: {
+        method : function(user){
+          console.log('Incrementing...');
+          return simulatePromiseDelay(function(){
+            user.counter += 1;
+          });
+        },
+        reload: false
+      },
+      reset: function(user){
+        console.log('Resetting...');
+        return simulatePromiseDelay(function(){
+          user.counter = 0;
+        });
+      },
+      remove: {
+        method : function(user){
+          _.remove($window.DATA, { id: user.id });
+        },
+        dialog : {
+          templateUrl : '_delete_user_confirm.html',
+          params      : function(item){
+            return { username: item.email };
+          }
+        }
+      }
+    }
+  };
+
+
+});
