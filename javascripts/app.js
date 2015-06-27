9b2a215e1b82d92c94b303dd238b35e8528e75a9
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-06-27 19:37:56 -0300

    Various fixes

--- javascripts/app.js
+++ javascripts/app.js
@@ -8,6 +8,15 @@ var app = angular.module('ngTableAsyncPages', [
   'CustomNoDataExample'
 ]);
 
+app.config(function($locationProvider){
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    rewriteLinks: false
+  });
+
+});
+
 app.run(function($rootScope, $window, $q, $timeout){
 
   $rootScope.simulateDelay = $window.simulateDelay($q, $timeout);
