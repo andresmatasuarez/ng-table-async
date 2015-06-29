ccb171b3562b87564c981c66bf703c73e14cd417
Andrés Mata Suárez <andresmata@zauberlabs.com>
2015-06-29 03:49:54 -0300

    Correct base tag for production

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,6 +10,22 @@ var app = angular.module('ngTableAsyncPages', [
   'DialogExample'
 ]);
 
+app.config(function($httpProvider, $locationProvider){
+
+  $locationProvider.html5Mode(true);
+
+  $httpProvider.interceptors.push(function($q){
+    return {
+      'request': function(config){
+        console.log('template', config.url);
+        // config.url = config.url.replace('partials/', 'ng-table-async/partials/');
+        return config;
+      },
+    };
+  });
+
+});
+
 app.run(function($rootScope, $window, $q, $timeout, $document){
 
   $rootScope.simulateDelay = $window.simulateDelay($q, $timeout);
